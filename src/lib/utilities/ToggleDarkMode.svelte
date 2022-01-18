6ac7704403c9001e0d986968d2f9688b0fe8657e
WormCrew <wayne.morgan.uk@gmail.com>
2022-01-18 20:46:48 +0000

    Update ToggleDarkMode.svelte

--- src/lib/utilities/ToggleDarkMode.svelte
+++ src/lib/utilities/ToggleDarkMode.svelte
@@ -7,15 +7,21 @@
 	};
 </script>
 
-<button id="night-mode-label" class="text-skin-inverted" on:click={toggleTheme}>
+<button
+	id="night-mode"
+	aria-label="night-mode-switch"
+	class="text-skin-inverted"
+	on:click={toggleTheme}
+>
 	<div
 		class="flex justify-center w-8 h-8 mr-2 align-middle border-2 rounded-full border-skin-border focus:outline-none"
-		aria-labelledby="night-mode-label"
 	>
 		{#if $theme === Theme.Dark}
-			<span> <i class="text-yellow-400 fa fa-sun-o" id="toggle-switch" /></span>
+			<span>
+				<i class="text-yellow-400 fa fa-sun-o" id="light-mode" aria-label="light-mode" /></span
+			>
 		{:else}
-			<span> <i class="text-gray-300 fa fa-moon-o" id="toggle-switch" /></span>
+			<span> <i class="text-gray-300 fa fa-moon-o" id="dark-mode" aria-label="dark-mode" /></span>
 		{/if}
 	</div>
 </button>
