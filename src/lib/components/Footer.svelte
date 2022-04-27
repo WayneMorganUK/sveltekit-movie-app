e8be47310854c873ca7f8cfe3e988fbe84ded8f6
WormCrew <wayne.morgan.uk@gmail.com>
2022-04-27 18:32:12 +0100

    Update Footer.svelte
    added responsive styling to footer

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,22 +1,28 @@
 <script lang="ts">
 </script>
 
-<header class="fixed bottom-0 z-50 w-full h-6 bg-skin-primary">
-	<div class="flex items-center justify-between h-6 pl-2 pr-1 mx-auto max-w-7xl md:pl-5 lg:pl-7 ">
-		<div class="text-skin-inverted flex flex-row items-center">
-			Powered by <a
-				class="pl-1 text-cyan-300 hover:text-skin-selected"
-				href="https:\\athena-designs.com"
-				target="_blank"
-				rel="external">Athena-Designs</a
-			>
+<footer class="fixed bottom-0 z-50 w-full bg-skin-primary">
+	<div class="max-w-7xl mx-auto flex flex-col md:flex-row md:justify-between ">
+		<div
+			class="flex flex-row md:flex-shrink items-center justify-between h-6 pl-2 pr-1 mx-auto max-w-7xl md:pl-5 lg:pl-7 "
+		>
+			<div class=" text-xs md:text-base text-skin-inverted flex flex-row items-center">
+				Powered by <a
+					class=" text-xs md:text-base pl-1 text-cyan-300 hover:text-skin-selected"
+					href="https:\\athena-designs.com"
+					target="_blank"
+					rel="external">Athena-Designs</a
+				>
+			</div>
 		</div>
 
-		<div class=" text-sm text-skin-inverted flex flex-row items-center">
-			<p>This product uses the TMDB</p>
+		<div
+			class=" text-xs md:text-base text-skin-inverted md:flex-grow flex flex-wrap justify-center md:justify-end md:pr-2 items-center"
+		>
+			<p>This product uses the</p>
 
 			<svg
-				class="h-[14px] px-2"
+				class="h-[8px] md:h-[12px] px-2"
 				xmlns="http://www.w3.org/2000/svg"
 				xmlns:xlink="http://www.w3.org/1999/xlink"
 				viewBox="0 0 273.42 35.52"
@@ -48,4 +54,4 @@
 			<p>API but is not endorsed or certified by TMDB</p>
 		</div>
 	</div>
-</header>
+</footer>
